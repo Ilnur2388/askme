ef7e7fc808b40efbc8c0feea5fe389cb65fc0d30
Ilnur Nurumov <ilnur.nurumov@mai.ru>
2023-02-11 16:43:12 -0500

    edit user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,6 @@ class UsersController < ApplicationController
   end
 
   def create
-    user_params = params.require(:user).permit(:name, :nickname, :email, :password)
 
     @user = User.new(user_params)
 
@@ -16,4 +15,27 @@ class UsersController < ApplicationController
       render :new
     end
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+  
+    if @user.update(user_params)
+      redirect_to root_path, notice: 'Данные пользователя обновлены!'
+    else
+      flash.now[:alert] = 'При попытке изменить пользователя возникли ошибки!'
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+    :name, :nickname, :email, :password, :password_confirmation)
+  end
+
 end
